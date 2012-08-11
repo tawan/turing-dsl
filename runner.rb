8a29c3ec29905a9cc1a3244da8331f5fc040ec3a
Tawan Sierek <tawan.sierek@gmail.com>
2012-08-11 23:12:26 +0200

    Added executable

---
+++ runner.rb
@@ -0,0 +1,32 @@
+require 'optparse'
+require './lib/io/cli.rb'
+require 'rubygems'
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: ruby runner.rb [options] [file ..]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-t TAPE", "--tape TAPE", "The machine will operate on this tape") do |tape|
+    options[:tape] = tape
+  end
+
+  opts.on("-s SPEED", "--speed SPEED", "The machine will operate in given speed (default is 1)") do |speed|
+    options[:speed] = speed
+  end
+  
+end.parse!
+Signal.trap("INT") {puts "prodded me" ; exit}
+
+
+p options
+file = ARGV[0]
+puts file
+require file
+m = Machine.find
+m.extend CLI
+m.speed = options[:speed].to_f unless options[:speed].nil?
+m.enter(m.initial_state)
+
