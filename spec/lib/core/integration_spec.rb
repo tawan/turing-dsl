c62129543dba467c65aaaff8c12c34d5664590ec
Tawan Sierek <tawan.sierek@gmail.com>
2012-08-21 20:43:57 +0200

    Added integration test that executes sample implementation

--- spec/lib/core/integration_spec.rb
+++ spec/lib/core/integration_spec.rb
@@ -11,3 +11,52 @@ describe "Integration of" do
     end
   end
 end
+
+describe "Sample implementation" do
+  it "should adds two numbers" do
+    Machine.defined_as(:adder) do
+      head_at_position(3).of Tape.with_blank_symbol("0").and_input_symbols("1") { "01101100" }
+
+      in_state 1 do
+        on "0" do
+          write "1"
+          move_right
+          enter 2
+        end
+
+        on "1" do
+          move_right
+          enter 1
+        end
+      end
+
+      in_state 2 do
+        on "0" do
+          move_left
+          enter 3
+        end
+
+        on "1" do
+          move_right
+          enter 2
+        end
+      end
+
+      in_state 3 do
+        on "0" do
+        end
+
+        on "1" do
+          erase
+        end
+      end
+    end
+
+    adder = Machine.find(:adder)
+    adder.tape.to_s.should eq("001101100")
+
+    adder.enter adder.initial_state 
+
+    adder.tape.to_s.should eq("001111000")
+  end
+end
